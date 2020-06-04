26ad590e1f74fe11ccf3a60c0c13946930a29993
Warner Hoefakker <warner.hoefakker@live.nl>
2020-06-04 14:23:26 +0200

    Mappenstructuur

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+// import logo from './logo.svg';
+import './sass/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           De Binnentuin
         </p>
