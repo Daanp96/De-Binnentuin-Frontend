f6e399a4b5d1fbbb9b50eacb257d4159e0353dfc
s1115477 <s1115477@student.hsleiden.n>
2020-06-08 11:14:39 +0200

    begin Menu edit

--- src/Admin.js
+++ src/Admin.js
@@ -3,13 +3,14 @@ import React from 'react';
 import OrderList from './OrderList';
 import TimeslotList from './TimeslotList';
 import OpeningsbordSection from './OpeningsbordSection';
+import AdminMenu from './AdminMenu';
 
 
 class Admin extends React.Component{
   render(){
     return(
 
-      <TimeslotList />
+      <AdminMenu />
     )
   }
 
---
+++ src/AdminMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AdminMenu = () =>{
+  return(
+    <section className="AdminMenuSection">
+      <h1> Menu </h1>
+
+      <button className="AdminMenuSection__button">Binnentuin Menu</button>
+      <button className="AdminMenuSection__button">Roof menu</button>
+      <button className="AdminMenuSection__button">Dranken menu</button>
+    </section>
+  )
+}
+
+export default AdminMenu;
