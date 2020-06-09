9eaf692a4afa074ef89b031e44d379161521fe80
Thomas Soeterbroek <thosoevel@gmail.com>
2020-06-09 12:16:21 +0200

    Kan nu de tijd ophalen in de opening component, en nog een standaard opzet voor de rest van de pagina

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './sass/App.scss';
+import './sass/main.css';
+import Maincontent from "./Maincontent.js"
+import Opening from "./Opening.js"
+import WeatherCard from './WeatherCard.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          De Binnentuin
-        </p>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+class App extends React.Component {
+  state = {time: "", isOpen1: "true" , isOpen2: "false"}
+
+  fetchPage(){
+    const Base_URL = "http://127.0.0.1:8000";
+    axios.get(Base_URL + "/landing").then(res => {
+       this.setState({
+          time: res.data[0] + "-" + res.data[1],
+       });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchPage();
+  }
+
+  render(){
+    return (
+      <article className="App">
+        <header className="App-header">
+          <h1>
+            De Binnentuin
+          </h1>
+            <Maincontent/>
+            <Opening img1Src ="./images/open-sign.png" imgName1 ="De Binnentuin is open" img2Src ="./images/closed-sign.png" imgName2 ="Het dakterras is dicht" time={this.state.time}/>
+            <WeatherCard/>
+
+        </header>
+      </article>
+    );
+  }
+
 }
+
+
 
 export default App;
---
+++ src/Maincontent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Maincontent = (props) => {
+  return (
+    <article>
+      <p> hier komt text en een button</p>
+    </article>
+  );
+}
+
+  export default Maincontent;
