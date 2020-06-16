86d352b6332dd837f14a5d2f8fe98fbacf9a35b3
Lukas <lukas.splinter@gmail.com>
2020-06-16 15:51:35 +0200

    table selection working, taking table id, location and requesting table availability per day/timeslot basis, missing styling for now

--- src/TafelCard.js
+++ src/TafelCard.js
@@ -3,27 +3,97 @@ import "./sass/TafelCard.scss";
 import axios from "axios";
 
 class TafelCard extends React.Component{
+  state = {day: "", month:"", year:""};
 
-  checkTableAvailable = () =>{
-    let tafelid = 1;
-    let date = "/31/01/2020";
-    const BASE_URL = "http://localhost:8000/api/timeslots/tafel/";
-    console.log(BASE_URL  + tafelid + date);
-    axios.get(BASE_URL  + tafelid + date).then(res=>console.log(res.data));
+  timeslots;
+
+  componentDidMount(){
+    let today = new Date();
+    let currentDate = {day:"", month:"", year:""};
+
+    if (today.getDate() < 10){
+      currentDate.day = "0" + today.getDate().toString();
+    }
+    else{
+      currentDate.day = today.getDate().toString();
+    }
+
+    if ((today.getMonth()+1) < 10){
+      currentDate.month = "0" + (today.getMonth()+1).toString();
+    }else{
+      currentDate.day = (today.getMonth()+1).toString();
+    }
+
+    currentDate.year = (today.getYear()+1900).toString();
+
+
+    this.setState({day:currentDate.day, month:currentDate.month, year:currentDate.year});
+  }
+
+  openDropdown = (event) =>{
+    let dropdown = event.target.parentNode.parentNode.childNodes[1];
+    let buttonText = event.target.childNodes[0];
+
+    buttonText.classList.toggle("tafelcard__head__continue__text--selected");
+    dropdown.classList.toggle("tafelcard__dropdown--open");
+
+    this.updateTimeslots("31", "01", this.state.year);
+  }
+
+  selectedDate = (event) =>{
+    let dateInput = event.target.value.split("-");
+
+    if (dateInput.length === 3) {
+      let formattedDateInput = {day:dateInput[2],month:dateInput[1],year:dateInput[0]};
+
+      this.updateTimeslots(formattedDateInput.day, formattedDateInput.month, formattedDateInput.year, this.props.tafelId);
+    }
+  }
+
+  updateTimeslots = (day, month, year, tableId) =>{
+    const BASE_URL = "http://localhost:8000/api/timeslots/";
+    let takenTimeslots = [];
+    let availableTimeslots = [];
+
+    axios.get(BASE_URL + "tafel/" + this.props.tafelId + "/" + day + "/" + month + "/" + year).then(res=>{
+      res.data.map(timeslot =>{takenTimeslots.push(timeslot.timeslots_id)});
+
+      axios.get(BASE_URL).then(res=>{
+        res.data.map(timeslot =>{
+          if (takenTimeslots.indexOf(timeslot.id) === -1) {
+            availableTimeslots.push(timeslot);
+          }
+        })
+
+        this.timeslots = availableTimeslots.map(timeslot =><option key={timeslot.TimeStart + timeslot.TimeStop}>{timeslot.TimeStart} - {timeslot.TimeStop}</option>);
+        this.forceUpdate();
+      });
+    });
+
   }
 
   render(){
     return(
       <section className="tafelcard">
-        <article className="tafelcard__infoContainer">
-          <p className="tafelcard__infoContainer__text tafelcard__infoContainer__text--label">Tafelnr</p>
-          <p className="tafelcard__infoContainer__text">{this.props.tafelNummer}</p>
-        </article>
-        <article className="tafelcard__infoContainer">
-          <p className="tafelcard__infoContainer__text tafelcard__infoContainer__text--label">Aantal plekken</p>
-          <p className="tafelcard__infoContainer__text">{this.props.maxAantalPersonen}</p>
-        </article>
-        <button className="tafelcard__continue" onClick={this.checkTableAvailable}>></button>
+        <section className="tafelcard__head">
+          <article className="tafelcard__head__infoContainer">
+            <p className="tafelcard__head__infoContainer__text tafelcard__head__infoContainer__text--label">Tafelnr</p>
+            <p className="tafelcard__head__infoContainer__text">{this.props.tafelNummer}</p>
+          </article>
+          <article className="tafelcard__head__infoContainer">
+            <p className="tafelcard__head__infoContainer__text tafelcard__head__infoContainer__text--label">Aantal plekken</p>
+            <p className="tafelcard__head__infoContainer__text">{this.props.maxAantalPersonen}</p>
+          </article>
+          <button className="tafelcard__head__continue" onClick={this.openDropdown}>
+            <p className="tafelcard__head__continue__text">></p><
+          /button>
+        </section>
+        <section className="tafelcard__dropdown">
+          <input type="date" onBlur={this.selectedDate}/>
+          <select>
+            {this.timeslots}
+          </select>
+        </section>
       </section>
     );
   }
