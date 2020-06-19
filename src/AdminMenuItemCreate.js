295176fc6c556c61f5af34e3edc2d1e0a1d00fa2
s1115477 <s1115477@student.hsleiden.n>
2020-06-19 15:19:08 +0200

    keuze voor welke restaurant

--- src/AdminMenuItemCreate.js
+++ src/AdminMenuItemCreate.js
@@ -7,7 +7,15 @@ class AdminMenuItemEdit extends React.Component{
 
   constructor(props){
     super(props);
-    this.state = {naam: "", prijs: "", beschrijving: "Tekst", categorie: "", id:"", special:""}
+    this.state = {naam: "",
+      prijs: "",
+      beschrijving: "Tekst",
+      categorie: "",
+      id:"",
+      special:"",
+      binnentuin: false,
+      dakterras: false
+    }
   }
 
   componentDidMount(){
@@ -30,6 +38,14 @@ class AdminMenuItemEdit extends React.Component{
     console.log(event.target.checked);
      this.setState({special: event.target.checked});
   }
+  handleChangeBinnen(event){
+    console.log(event.target.checked);
+    this.setState({binnentuin: event.target.checked});
+  }
+  handleChangeDakterras(event){
+    console.log(event.target.checked);
+    this.setState({dakterras: event.target.checked});
+  }
 
   handleSubmit = (event) =>{
     event.preventDefault();
@@ -37,7 +53,15 @@ class AdminMenuItemEdit extends React.Component{
     axios({
       method:'post',
       url: 'http://localhost:8000/api/admin/menuitem/create',
-      data: {id: this.state.id, naam:this.state.naam, beschrijving:this.state.beschrijving, prijs:this.state.prijs, categorie:this.state.categorie, special:this.state.special},
+      data: {id: this.state.id,
+        naam:this.state.naam,
+        beschrijving:this.state.beschrijving,
+        prijs:this.state.prijs,
+        categorie:this.state.categorie,
+        special:this.state.special,
+        binnentuin:this.state.binnentuin,
+        dakterras:this.state.dakterras
+      },
     });
   }
 
@@ -48,11 +72,11 @@ class AdminMenuItemEdit extends React.Component{
         <form className="MenuEditBox__Form" onSubmit={this.handleSubmit}>
           <label >
             Naam:
-            <input type="text" id="naam" name="naam"   onChange={(event) => this.handleChangeNaam(event)}/>
+            <input required type="text" id="naam" name="naam"   onChange={(event) => this.handleChangeNaam(event)}/>
            </label>
           <label >
             Prijs:
-            <input type="number" id="prijs" name="prijs" step="0.01"  onChange={(event) => this.handleChangePrijs(event)}/>
+            <input required type="number" id="prijs" name="prijs" step="0.01"  onChange={(event) => this.handleChangePrijs(event)}/>
           </label>
 
           <label >
@@ -62,11 +86,19 @@ class AdminMenuItemEdit extends React.Component{
 
           <label >
             Categorie:
-          <input type="text" id="categorie" name="categorie"   onChange={(event) => this.handleChangeCategorie(event)}/>
+          <input required type="text" id="categorie" name="categorie"   onChange={(event) => this.handleChangeCategorie(event)}/>
         </label>
         <label>
           Chef's special:
           <input type="checkbox" id="special" name="special"  onChange={(event) => this.handleChangeSpecial(event)}/>
+        </label>
+        <label>
+          Restaurant Binnentuin:
+          <input type="checkbox" id="binnentuin" name="binnentuin"  onChange={(event) => this.handleChangeBinnen(event)}/>
+        </label>
+        <label>
+          Restaurant Dakterras:
+          <input type="checkbox" id="dakterras" name="dakterras"  onChange={(event) => this.handleChangeDakterras(event)}/>
         </label>
 
         <input type="submit" value="Veranderen"/>
