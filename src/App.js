36e445fdf5d287aea6e4fd2e751c1765241a4cb0
Thomas Soeterbroek <thosoevel@gmail.com>
2020-06-22 11:21:07 +0200

    Begin van router en weather component van daan erin gestopt

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import React from 'react';
 import './sass/main.css';
 import Maincontent from "./Maincontent.js"
 import Opening from "./Opening.js"
-import WeatherCard from './WeatherCard.js'
+import Weather from './Weather.js'
 
 import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends React.Component {
   state = {time: "", isOpen1: "true" , isOpen2: "false"}
@@ -27,13 +33,16 @@ class App extends React.Component {
     return (
       <article className="App">
         <header className="App-header">
-          <h1>
-            De Binnentuin
-          </h1>
-            <Maincontent/>
-            <Opening img1Src ="./images/open-sign.png" imgName1 ="De Binnentuin is open" img2Src ="./images/closed-sign.png" imgName2 ="Het dakterras is dicht" time={this.state.time}/>
-            <WeatherCard/>
-
+          <Router>
+            <Route path="/">
+              <h1>
+                De Binnentuin
+              </h1>
+              <Maincontent/>
+              <Opening img1Src ="./images/open-sign.png" imgName1 ="De Binnentuin is open" img2Src ="./images/closed-sign.png" imgName2 ="Het dakterras is dicht" time={this.state.time}/>
+              <Weather/>
+            </Route>
+          </Router>
         </header>
       </article>
     );
--- src/Maincontent.js
+++ src/Maincontent.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Maincontent = (props) => {
   return (
     <article>
-      <p> hier komt text en een button</p>
+      <p>
+      De binnentuin en het dakterras zijn een goede plek om te ontspannen.
+      Iedereen kan hier naar binnen voor een lekkere maaltijd of om iets te komen drinken.
+      Als je interesse hebt, reserveer dan een plekje!
+      We zien je graag langskomen.
+      </p>
+      <button>Click hier</button>
     </article>
   );
 }
