fc423d9af4da2d5aaf5a8a0a8b35d39f1d447628
Daan <daan.passchier@xs4all.nl>
2020-06-24 13:58:00 +0200

    Styling voor menuitems

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -4,9 +4,11 @@ import "./sass/Menuitem.scss";
 const MenuItem = props => {
     return(
         <section className="menu_list__item">
-            <h2>{props.naam}</h2>
-            <p>{props.beschrijving}</p>
-            <p>€{props.prijs}</p>
+            <h2 className="menu_list__item__title">{props.naam}</h2>
+            <p className="menu_list__item__description">{props.beschrijving}</p>
+            <p className="menu_list__item__price">€{props.prijs}</p>
+            <button className="menu_list__item__cart">Add to cart</button>
+            <button className="menu_list__item__favorite">Add to favorites</button>
         </section>
     );
 }
