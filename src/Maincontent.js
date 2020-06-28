a821158432f8a85c4b35219d916e0212373b20f0
Thomas Soeterbroek <thosoevel@gmail.com>
2020-06-28 11:34:31 +0200

    button is nu gestyled en werkt beter

--- src/Maincontent.js
+++ src/Maincontent.js
@@ -6,13 +6,14 @@ import {
 
 const Maincontent = (props) => {
   return (
-    <article>
+    <article className="main_content">
       <h2 className="main_content__text">
         De binnentuin en het dakterras zijn een goede plek om te ontspannen.
         Iedereen kan hier naar binnen voor een lekkere maaltijd of om iets te komen drinken.
         Als je interesse hebt, reserveer dan een plekje!
         We zien je graag langskomen.
       </h2>
+      <Link to="/reserveren"><button className="main_content__button">Reserveer hier</button></Link>
     </article>
   );
 }
