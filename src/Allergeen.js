430e59cf073393c26ad1d594e44a4f20797052f0
lukas splinter <lukas.splinter@gmail.com>
2020-07-02 10:23:08 +0200

    added allergeen.js + allergeen.scss

---
+++ src/Allergeen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./sass/Allergeen.scss";
+
+const Allergeen = (props) =>{
+  let source;
+  let classes;
+  if (props.line === "true") {
+    source = "./allergenenIMG/" + props.allergy + "_line.png";
+    classes = "allergeen-icoon-container__icoon";
+  }else {
+    source = "./allergenenIMG/" + props.allergy + ".png";
+    classes = "allergeen-icoon-container__icoon allergeen-icoon-container__icoon--colored";
+  }
+
+  return(
+    <section className="allergeen-icoon-container">
+      <section className={classes}>
+        <img className="allergeen-icoon-container__icoon__img" src={source} alt={props.allergy + " allergy warning"}/>
+      </section>
+      <p className="allergeen-icoon-container__text">{"contains " + props.allergy}</p>
+    </section>
+
+  );
+}
+
+export default Allergeen;
