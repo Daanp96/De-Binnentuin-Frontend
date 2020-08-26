2fe1c153cade08f60b1fcd098c93b0ef3b05e6af
MarcodenBoer <marco.denboer2000@gmail.com>
2020-08-26 15:37:14 +0200

    Route naar 404 gemaakt en 404 bestand aangemaakt

--- src/App.js
+++ src/App.js
@@ -30,6 +30,8 @@ import Twitter from "./Twitter";
 
 import Header from "./Header";
 
+import Error404 from './Error404';
+
 import axios from "axios";
 
 import {
@@ -269,12 +271,16 @@ class App extends React.Component {
                       <TimeslotList />
                     </section>
                   </Route>
+                  <Route path = "/404">
+                    <Error404/>
+                  </Route>
 
                 <Route path="/">
                   <Maincontent/>
                   <Opening img1Src ={this.state.isOpen1} img2Src ={this.state.isOpen2} timeStart={this.state.timeStart} timeEnd={this.state.timeEnd}/>
                   <Weather/>
                 </Route>
+
             </Switch>
           </Router>
         </main>
---
+++ src/Error404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './sass/App.scss';
+
+class Error404 extends React.Component{
+
+  render(){
+    return(
+      <section>
+        
+      </section>
+    );
+  }
+}
+
+export default Error404;
